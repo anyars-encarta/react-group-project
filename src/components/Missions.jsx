c6258c4bc0a5e655f1729067df54d18cc8901873
Anyars Yussif <anyarsencarta@gmail.com>
2023-09-12 07:58:39 +0000

    Implement basic structure for missions

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,6 +1,59 @@
 const Missions = () => (
   <div className="missions">
-    <h2>Mission Data Goes Here</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th> </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Missions</td>
+          <td>
+            The Missions section displays a list of current missions
+            along with their brief description and participation status.
+            There is also a button next to each mission that allows users
+            to join the selected mission or leave the mission the user
+            joined earlier.
+          </td>
+          <td>NOT A MEMBER</td>
+          <td>
+            <button type="button">Leave Mission</button>
+          </td>
+        </tr>
+        <tr>
+          <td>Missions</td>
+          <td>
+            The Missions section displays a list of current missions
+            along with their brief description and participation status.
+            There is also a button next to each mission that allows users
+            to join the selected mission or leave the mission the user
+            joined earlier.
+          </td>
+          <td>NOT A MEMBER</td>
+          <td>
+            <button type="button">Leave Mission</button>
+          </td>
+        </tr>
+        <tr>
+          <td>Missions</td>
+          <td>
+            The Missions section displays a list of current missions
+            along with their brief description and participation status.
+            There is also a button next to each mission that allows users
+            to join the selected mission or leave the mission the user
+            joined earlier.
+          </td>
+          <td>NOT A MEMBER</td>
+          <td>
+            <button type="button">Leave Mission</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 );
 
