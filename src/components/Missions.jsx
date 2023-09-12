191bb80427f7ab4888444155be953904446a7741
Anyars Yussif <anyarsencarta@gmail.com>
2023-09-12 12:28:59 +0000

    Add loading state to fetch data

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import fetchMissions from '../redux/missions/missionsSlice';
 
@@ -6,32 +6,44 @@ const Missions = () => {
   const missions = useSelector((state) => state.missions.missions);
   const dispatch = useDispatch();
 
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    dispatch(fetchMissions());
+    dispatch(fetchMissions())
+      .then(() => {
+        setLoading(false);
+      })
+      .catch((error) => {
+        throw new Error('Error fetching missions:', error);
+      });
   }, [dispatch]);
 
   return (
     <div className="missions">
-      <table>
-        <thead>
-          <tr>
-            <th>Mission</th>
-            <th>Description</th>
-            <th>Status</th>
-            <th> </th>
-          </tr>
-        </thead>
-        <tbody>
-          {missions.map((mission) => (
-            <tr key={mission.mission_id}>
-              <td>{mission.mission_name}</td>
-              <td>{mission.description}</td>
-              <td>{mission.status}</td>
-              <td><button type="button">Leave Mission</button></td>
+      {loading ? (
+        <p>Loading missions...</p>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>Mission</th>
+              <th>Description</th>
+              <th>Status</th>
+              <th> </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {missions.map((mission) => (
+              <tr key={mission.mission_id}>
+                <td>{mission.mission_name}</td>
+                <td>{mission.description}</td>
+                <td>{mission.status}</td>
+                <td><button type="button">Leave Mission</button></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
