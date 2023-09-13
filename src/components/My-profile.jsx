870868bc243959881880687e5d83ff68d618c940
Anyars Yussif <anyarsencarta@gmail.com>
2023-09-13 11:30:17 +0000

    Add extra leave mission and read more to missions

--- src/components/My-profile.jsx
+++ src/components/My-profile.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import '../styles/my-profile.css';
+import { leaveMission } from '../redux/missions/missionsSlice';
 
 const MyProfile = () => {
   const msj = useSelector((state) => state.missions.missions.filter((mission) => mission.reserved));
+  const dispatch = useDispatch();
+
+  const redirectToWikipedia = (url) => {
+    window.open(url, '_blank');
+  };
 
   return (
     <div className="my-profile">
@@ -15,9 +21,21 @@ const MyProfile = () => {
             </tr>
           </thead>
           <tbody>
+            {msj.length === 0 && <tr><td>No missions joined</td></tr>}
             {msj.map((mj) => (
               <tr key={mj.mission_id}>
-                <td>{mj.mission_name}</td>
+                <td className="joined-mission">
+                  {mj.mission_name}
+                  <div className="mission-buttons">
+                    <button type="button" className="read-more" onClick={() => redirectToWikipedia(mj.wikipediaUrl)}>
+                      Read More
+                    </button>
+                    <button type="button" className="leave-m" onClick={() => dispatch(leaveMission(mj.mission_id))}>
+                      Leave Mission
+                    </button>
+                  </div>
+                </td>
+
               </tr>
             ))}
           </tbody>
