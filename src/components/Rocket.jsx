ee0658ce2baeaa97435d3d6de592b8aa272e5af3
BiancaMe <bianquis0410@gmail.com>
2023-09-12 23:32:31 -0300

    Fetch rockets, add basic style to rockets section

---
+++ src/components/Rocket.jsx
@@ -0,0 +1,17 @@
+const Rocket = () => (
+  <div className="rocket">
+    <img alt="img" />
+    <div className="rocket-info">
+      <h2 className="rocket-title">Example 1</h2>
+      <p className="rocket-description">
+        sdfsfffffffffffffffffffsdfsdfsd dfggggggggggg
+        ggggggggggggggd
+        dfgggggggggggggggggg dfggggggggggggg dfggg
+        gggggg dfgggggg
+      </p>
+      <button type="button" className="btn-reserve">Reserve Rocket</button>
+    </div>
+  </div>
+);
+
+export default Rocket;
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,10 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import missionsReducer from './reducers/missionsReducer';
+import rocketsReducer from './rockets/rocketsSlice';
 
 // Combine reducers
 const rootReducer = combineReducers({
   missions: missionsReducer,
+  rockets: rocketsReducer,
 });
 
 // Apply middleware
