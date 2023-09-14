44587a53b97542a127688aecc9efff7f6c88eed0
BiancaMe <bianquis0410@gmail.com>
2023-09-13 21:11:26 -0300

    Add Reserve Rocket functionality

--- src/components/Rocket.jsx
+++ src/components/Rocket.jsx
@@ -1,17 +1,25 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { reserveRocket } from '../redux/rockets/rocketsSlice';
 
-const Rocket = ({ rocket }) => (
-  <div className="rocket">
-    <img alt="img" className="rocket-img" src={rocket.flickr_images[0]} />
-    <div className="rocket-info">
-      <h2 className="rocket-title">{rocket.name}</h2>
-      <p className="rocket-description">
-        {rocket.description}
-      </p>
-      <button type="button" className="btn-reserve">Reserve Rocket</button>
+const Rocket = ({ rocket }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="rocket">
+      <img alt="img" className="rocket-img" src={rocket.flickr_images[0]} />
+      <div className="rocket-info">
+        <h2 className="rocket-title">{rocket.name}</h2>
+        <p className="rocket-description">
+          {rocket.reserved ? (
+            <span className="rocket-reserved">Reserved </span>) : ('')}
+          {rocket.description}
+        </p>
+        <button type="button" className="btn-reserve" onClick={() => dispatch(reserveRocket(rocket.id))}>Reserve Rocket</button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 Rocket.propTypes = {
   rocket: PropTypes.shape({
@@ -19,6 +27,7 @@ Rocket.propTypes = {
     name: PropTypes.string,
     description: PropTypes.string,
     flickr_images: PropTypes.arrayOf(PropTypes.string),
+    reserved: PropTypes.bool,
   }),
 };
 
