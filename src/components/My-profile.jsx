c8617be642cc58397a8766cb85935f02068d48fa
BiancaMe <bianquis0410@gmail.com>
2023-09-14 12:09:21 -0300

    Display rockes in my perfil

--- src/components/My-profile.jsx
+++ src/components/My-profile.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import '../styles/my-profile.css';
 import { leaveMission } from '../redux/missions/missionsSlice';
+import { cancelationRocket } from '../redux/rockets/rocketsSlice';
 
 const MyProfile = () => {
   const msj = useSelector((state) => state.missions.missions.filter((mission) => mission.reserved));
+  const rockets = useSelector((state) => state.rockets.rockets.filter((rocket) => rocket.reserved));
   const dispatch = useDispatch();
 
   const redirectToWikipedia = (url) => {
@@ -50,20 +52,15 @@ const MyProfile = () => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>
-                Video provides a powerful way to help you prove your point.
-                When you click Online Video, you can paste in the embed code
-                for the video you want to add. You can also type a keyword to
-                search online for the video that best fits your document.
-              </td>
-            </tr>
-            <tr>
-              <td>Falcon 2</td>
-            </tr>
-            <tr>
-              <td>Falcon 3</td>
-            </tr>
+            {rockets.length === 0 && <tr><td>No missions joined</td></tr>}
+            {rockets.map((rocket) => (
+              <tr key={rocket.id}>
+                <td className="td-rocket">
+                  {rocket.name}
+                  <button type="button" className="btn-cancel" onClick={() => dispatch(cancelationRocket(rocket.id))}>Cancel Reservation</button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
