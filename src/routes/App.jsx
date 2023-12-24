90d55eaae1a747b0e675ef3e37bf61cc0d99376b
anyars-encarta <anyarsencarta@gmail.com>
2023-12-24 15:17:30 +0000

    Correct routes

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Rockets from '../components/Rockets';
 import MyProfile from '../components/My-profile';
 import Missions from '../components/Missions';
